bbf3a71dbae7e47206cabe7662fdc0da4da6a22b
Thijs Jonkman <t.jonkman@gmail.com>
2013-06-20 13:49:23 +0200

    Removed tests, txt changed to md

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='pareto.portlet.twittertimeline',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.md").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
