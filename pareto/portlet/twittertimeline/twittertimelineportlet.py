f7714d2a37b12c1c4a4344c30494dd0c34369ebf
Maurits van Rees <maurits@vanrees.org>
2015-12-17 19:26:15 +0100

    autopep8safe

--- pareto/portlet/twittertimeline/twittertimelineportlet.py
+++ pareto/portlet/twittertimeline/twittertimelineportlet.py
@@ -27,7 +27,7 @@ class ChromeVocabulary(object):
 
     def __call__(self, context):
         items = [
-            "noheader", "nofooter", "noborders", "noscrollbar","transparent"]
+            "noheader", "nofooter", "noborders", "noscrollbar", "transparent"]
         items = [SimpleTerm(i, i, i) for i in items]
         return SimpleVocabulary(items)
 
@@ -38,17 +38,19 @@ class InvalidHexColor(ValidationError):
     __doc__ = _(u"This is an invalid color code. "
                 u"It must be a html hex color code e.g. #000000")
 
+
 def validate_hex_color(value):
     if value == u"":
         return True
     if len(value) != 7 or not re.match(
-        '^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', value):
+            '^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', value):
         raise InvalidHexColor(value)
     return True
 
 
 class InvalidInt(ValidationError):
     __doc__ = _(u"This is not a valid integer, .e.g. 3")
+
 
 def validate_int(value):
     if value == u"":
@@ -58,6 +60,7 @@ def validate_int(value):
     except ValueError:
         raise InvalidInt(value)
     return True
+
 
 class ITwitterTimelinePortlet(IPortletDataProvider):
     """A portlet
@@ -69,7 +72,7 @@ class ITwitterTimelinePortlet(IPortletDataProvider):
     header = schema.TextLine(
         title=_(u"Portlet header"),
         description=_(u"Title of the rendered portlet. Only shows when "
-            u"emulate portlet is enabled."),
+                      u"emulate portlet is enabled."),
         required=True)
 
     username = schema.TextLine(
@@ -123,21 +126,21 @@ class ITwitterTimelinePortlet(IPortletDataProvider):
     chrome = schema.List(
         title=_(u"Chrome"),
         description=_(u"Control the timeline layout and chrome. "
-            u"You can use a set of the following options: "
-            u" "
-            u"noheader: Hides the timeline header. Please refer to the "
-            u"timeline display requirements when implementing your own header."
-            u" "
-            u"nofooter: Hides the timeline footer and Tweet box, if included."
-            u" "
-            u"noborders: Removes all borders within the timeline (between "
-            u"Tweets, cards, around the timeline.) See also: border-color."
-            u" "
-            u"noscrollbar: Crops and hides the main timeline scrollbar, if "
-            u"visible. Please consider that hiding standard user interface "
-            u"components can affect the accessibility of your website."
-            u" "
-            u"transparent: Removes the background color."),
+                      u"You can use a set of the following options: "
+                      u" "
+                      u"noheader: Hides the timeline header. Please refer to the "
+                      u"timeline display requirements when implementing your own header."
+                      u" "
+                      u"nofooter: Hides the timeline footer and Tweet box, if included."
+                      u" "
+                      u"noborders: Removes all borders within the timeline (between "
+                      u"Tweets, cards, around the timeline.) See also: border-color."
+                      u" "
+                      u"noscrollbar: Crops and hides the main timeline scrollbar, if "
+                      u"visible. Please consider that hiding standard user interface "
+                      u"components can affect the accessibility of your website."
+                      u" "
+                      u"transparent: Removes the background color."),
         value_type=schema.Choice(
             source='pareto.portlet.twittertimeline.Chrome'),
         required=False,
@@ -153,45 +156,45 @@ class ITwitterTimelinePortlet(IPortletDataProvider):
     lang = schema.TextLine(
         title=_(u"Language"),
         description=_(u"The timeline language is detected from the page, "
-            u"based on the HTML lang attribute of your content. You can also "
-            u"set the HTML lang attribute on the embed code itself."),
+                      u"based on the HTML lang attribute of your content. You can also "
+                      u"set the HTML lang attribute on the embed code itself."),
         required=False)
 
     tweet_limit = schema.TextLine(
         title=_(u"Tweet limit"),
         description=_(u"To fix the size of a timeline to a preset number of "
-            u"Tweets, set the tweet-limit with any number between 1 and 20 "
-            u"Tweets. The timeline will render the specified number of Tweets "
-            u"from the timeline, expanding the height of the timeline to "
-            u"display all Tweets without scrolling. Since the timeline is "
-            u"of a fixed size, it will not poll for updates when using this "
-            u"option."),
+                      u"Tweets, set the tweet-limit with any number between 1 and 20 "
+                      u"Tweets. The timeline will render the specified number of Tweets "
+                      u"from the timeline, expanding the height of the timeline to "
+                      u"display all Tweets without scrolling. Since the timeline is "
+                      u"of a fixed size, it will not poll for updates when using this "
+                      u"option."),
         required=False)
 
     related = schema.TextLine(
         title=_(u"Web Intent Related Users"),
         description=_(u"As per the Tweet and follow buttons, you may "
-            u"provide a comma-separated list of user screen names as "
-            u"suggested followers to a user after they reply, Retweet, or "
-            u"favorite a Tweet in the timeline."),
+                      u"provide a comma-separated list of user screen names as "
+                      u"suggested followers to a user after they reply, Retweet, or "
+                      u"favorite a Tweet in the timeline."),
         required=False)
 
     aria_polite = schema.Bool(
         title=_(u"ARIA politeness"),
         description=_(u"ARIA is an accessibility system that aids people "
-            u"using assistive technology interacting with dynamic web "
-            u"content. Read more about ARIA on W3C's website. If enabled, the "
-            u"embedded timeline uses the least obtrusive setting. If using an "
-            u"embedded timeline as a primary source of content on your page, "
-            u"you may wish to override this to the assertive setting."),
+                      u"using assistive technology interacting with dynamic web "
+                      u"content. Read more about ARIA on W3C's website. If enabled, the "
+                      u"embedded timeline uses the least obtrusive setting. If using an "
+                      u"embedded timeline as a primary source of content on your page, "
+                      u"you may wish to override this to the assertive setting."),
         required=False,
         default=True)
 
     emulate_portlet = schema.Bool(
         title=_(u"Emulate portlet"),
         description=_(u"If enabled, the timeline is set as unobtrusive as "
-            u"possible and rendered in a portlet instead of the standard "
-            u"Twitter timeline."),
+                      u"possible and rendered in a portlet instead of the standard "
+                      u"Twitter timeline."),
         required=False,
         default=False)
 
@@ -203,7 +206,7 @@ class ITwitterTimelinePortlet(IPortletDataProvider):
     more_url = schema.ASCIILine(
         title=_(u"Details link"),
         description=_(u"If given, the header and footer "
-            "will link to this URL."),
+                      "will link to this URL."),
         required=False)
 
 
@@ -234,11 +237,11 @@ class Assignment(base.Assignment):
     footer = u""
     more_url = ''
 
-    def __init__(self, header = u"", info = u"",
-        username = u"", timeline_id = u"", theme = u"", link_color = u"",
-        width = u"", height = u"", chrome = None, border_color = u"",
-        lang = u"", tweet_limit = u"", related = u"", aria_polite = True,
-        emulate_portlet = False, footer = u"", more_url = ''):
+    def __init__(self, header=u"", info=u"",
+                 username=u"", timeline_id=u"", theme=u"", link_color=u"",
+                 width=u"", height=u"", chrome=None, border_color=u"",
+                 lang=u"", tweet_limit=u"", related=u"", aria_polite=True,
+                 emulate_portlet=False, footer=u"", more_url=''):
 
         self.header = header
         self.info = info
@@ -304,7 +307,7 @@ class Renderer(base.Renderer):
             # be any encoding in the world.
             orig = unicode(orig, 'utf-8', 'ignore')
             logger.warn("Static portlet at %s has stored non-unicode text. "
-                "Assuming utf-8 encoding." % context.absolute_url())
+                        "Assuming utf-8 encoding." % context.absolute_url())
 
         # Portal transforms needs encoded strings
         orig = orig.encode('utf-8')
